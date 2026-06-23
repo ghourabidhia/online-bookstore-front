9bd3f24644b83f15fcc18160aedc4dff7703a64a
dhia <ghourabidhia@gmail.com>
2026-06-23 19:05:05 +0200

    feat(layout): add bootstrap, app entry point, and root component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { AuthProvider } from '@/context/AuthContext';
+import { CartProvider } from '@/context/CartContext';
+import { AppRouter } from '@/router/AppRouter';
+
+function App() {
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <AppRouter />
+      </CartProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
